2606a7c9bc148468437e32b1a484e317fe6113bc
zvxjwaa <70423086+ajsix@users.noreply.github.com>
2020-11-11 15:23:24 +0800

    Update dm.py

--- dm/dm.py
+++ dm/dm.py
@@ -6,10 +6,6 @@ class Dm(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        print(message.content)
-
     @commands.command()
     async def dm(ctx, userid: int,*, dm: str):
        boi = bot.get_user(userid)
