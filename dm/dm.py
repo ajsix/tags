5b58de44917bd8c7198dccb6c4a86ef78b435d89
zvxjwaa <70423086+ajsix@users.noreply.github.com>
2020-11-11 15:17:39 +0800

    Update dm.py

--- dm/dm.py
+++ dm/dm.py
@@ -6,15 +6,15 @@ class DmPlugin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-@command.commands()
-async def dm(ctx, userid: int,*, dm: str):
-    boi = bot.get_user(userid)
-    await boi.send(dm)
-    await ctx.send('DM sent!')
+    @commands.command()
+    async def dm(ctx, userid: int,*, dm: str):
+       boi = bot.get_user(userid)
+       await boi.send(dm)
+       await ctx.send('DM sent!')
 
-@command.commands()
-async def spam(ctx, userid: int,*, dm: str):
-    while True:
+    @commands.command()
+    async def spam(ctx, userid: int,*, dm: str):
+       while True:
         boi = bot.get_user(userid)
         await boi.send(dm)
         await ctx.send('DM sent!')
